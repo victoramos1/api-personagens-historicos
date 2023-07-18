722c1afcdc1b4ea81f7bcd42743c1f7cded015e3
victoramos1 <ramos_victor@outlook.com>
2023-07-18 20:41:38 -0300

    Atualização

--- database/db.js
+++ database/db.js
@@ -5,7 +5,7 @@ const sequelize = new Sequelize(
   `${process.env.DB_NAME}`,
   `${process.env.DB_USER}`,
   `${process.env.DB_PASSWORD}`, {
-  dialect: `${process.env.DB_DIALECT}`,
+  dialect: "mysql",
   host: `${process.env.DB_HOST}`,
   dialectOptions: {
     ssl: {
